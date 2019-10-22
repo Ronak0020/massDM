08fad1b73f02c79daa82915800954c8153b67272
Ronak0020 <49647021+Ronak0020@users.noreply.github.com>
2019-10-22 15:14:25 +0530

    Update app.js

--- app.js
+++ app.js
@@ -26,7 +26,7 @@ bot.on("ready", () => {
         setInterval(function() {
         let status = statuses[Math.floor(Math.random() * statuses.length)];
         bot.user.setActivity(status, {type: "PLAYING"});
-}, 5000)
+}, 3000)
 });
 
 
